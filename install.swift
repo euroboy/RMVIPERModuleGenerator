e1c8271942bcd230f721f79e7b299f9dcb53d1b8
euroboy <rmatei1@gmail.com>
2018-11-21 10:16:36 +0200

    Minor fixes

--- install.swift
+++ install.swift
@@ -8,31 +8,34 @@ import Foundation
 let templateName = "RMVIPER Module.xctemplate"
 let destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
 
-func printInConsole(_ message:Any){
+func printInConsole(_ message:Any)
+{
     print("====================================")
     print("\(message)")
     print("====================================")
 }
 
-func moveTemplate(){
-
+func moveTemplate()
+{
     let fileManager = FileManager.default
     let destinationPath = bash(command: "xcode-select", arguments: ["--print-path"]).appending(destinationRelativePath)
-    do {
-        if !fileManager.fileExists(atPath:"\(destinationPath)/\(templateName)"){
-
+    do 
+    {
+        if !fileManager.fileExists(atPath:"\(destinationPath)/\(templateName)")
+        {
             try fileManager.copyItem(atPath: templateName, toPath: "\(destinationPath)/\(templateName)")
-
             printInConsole("✅  Template installed succesfully 🎉. Enjoy it 🙂")
 
-        }else{
-
-            try _ = fileManager.replaceItemAt(URL(fileURLWithPath:"\(destinationPath)/\(templateName)"), withItemAt: URL(fileURLWithPath:templateName))
-
+        }
+        else
+        {
+            try fileManager.removeItem(atPath: "\(destinationPath)/\(templateName)")
+            try fileManager.copyItem(atPath: templateName, toPath: "\(destinationPath)/\(templateName)")
             printInConsole("✅  Template already exists. So has been replaced succesfully 🎉. Enjoy it 🙂")
         }
     }
-    catch let error as NSError {
+    catch let error as NSError 
+    {
         printInConsole("❌  Ooops! Something went wrong 😡 : \(error.localizedFailureReason!)")
     }
 }
@@ -49,7 +52,8 @@ func shell(launchPath: String, arguments: [String]) -> String
 
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: String.Encoding.utf8)!
-    if output.count > 0 {
+    if output.count > 0 
+    {
         //remove newline character.
         let lastIndex = output.index(before: output.endIndex)
         return String(output[output.startIndex ..< lastIndex])
@@ -57,7 +61,8 @@ func shell(launchPath: String, arguments: [String]) -> String
     return output
 }
 
-func bash(command: String, arguments: [String]) -> String {
+func bash(command: String, arguments: [String]) -> String 
+{
     let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
     return shell(launchPath: whichPathForCommand, arguments: arguments)
 }
