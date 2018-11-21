a2827a04ab893c07962b7edc947984bc5ce3a46e
euroboy <rmatei1@gmail.com>
2018-11-21 12:42:58 +0200

    Improved configurator

--- RMVIPER Module.xctemplate/Module/Configurator/___FILEBASENAME___ModuleConfigurator.swift
+++ RMVIPER Module.xctemplate/Module/Configurator/___FILEBASENAME___ModuleConfigurator.swift
@@ -10,13 +10,9 @@ import UIKit
 
 class ___VARIABLE_productName:identifier___ModuleConfigurator
 {
-    func configureModule(for viewController: ___VARIABLE_productName:identifier___ViewController)
+    class func configureModule() -> UIViewController
     {
-        configure(viewController: viewController)
-    }
-    
-    private func configure(viewController: ___VARIABLE_productName:identifier___ViewController)
-    {
+        let viewController = ___VARIABLE_productName:identifier___ViewController.instantiate()
         var presenter: ___VARIABLE_productName:identifier___PresenterProtocol & ___VARIABLE_productName:identifier___InteractorOutputProtocol = ___VARIABLE_productName:identifier___Presenter()
         var interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol = ___VARIABLE_productName:identifier___Interactor()
         let wireFrame: ___VARIABLE_productName:identifier___WireframeProtocol = ___VARIABLE_productName:identifier___Wireframe()
@@ -26,5 +22,7 @@ class ___VARIABLE_productName:identifier___ModuleConfigurator
         presenter.wireFrame = wireFrame
         presenter.interactor = interactor
         interactor.presenter = presenter
+        
+        return viewController
     }
 }
