ba7a1e1cd62f65a2f1c268c9dd5ad401fed10854
euroboy <rmatei1@gmail.com>
2019-04-12 10:29:33 +0300

    Minor improvement

--- RMVIPER Module.xctemplate/Module/___FILEBASENAME___ModuleConfigurator.swift
+++ RMVIPER Module.xctemplate/Module/___FILEBASENAME___ModuleConfigurator.swift
@@ -15,8 +15,8 @@ class ___VARIABLE_productName:identifier___ModuleConfigurator
     class func configureModule() -> UIViewController
     {
         let viewController = ___VARIABLE_productName:identifier___ViewController.instantiate()
-        var presenter: ___VARIABLE_productName:identifier___PresenterProtocol & ___VARIABLE_productName:identifier___InteractorOutputProtocol = ___VARIABLE_productName:identifier___Presenter()
-        var interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol = ___VARIABLE_productName:identifier___Interactor()
+        let presenter: ___VARIABLE_productName:identifier___PresenterProtocol & ___VARIABLE_productName:identifier___InteractorOutputProtocol = ___VARIABLE_productName:identifier___Presenter()
+        let interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol = ___VARIABLE_productName:identifier___Interactor()
         let wireFrame: ___VARIABLE_productName:identifier___WireframeProtocol = ___VARIABLE_productName:identifier___Wireframe()
         
         viewController.presenter = presenter
